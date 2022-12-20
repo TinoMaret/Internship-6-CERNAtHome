e4989008fd1cbac88cc4b1dad8a4d264479e8ec5
Tino Maretic <pripravnik-tino.maretic@dump.hr>
2022-12-20 22:07:03 +0100

    Changed some things about tables

--- CernAtHome.sql
+++ CernAtHome.sql
@@ -10,21 +10,22 @@ CREATE TABLE Projects (
 );
 
 CREATE TABLE AcceleratorProject(
-	AcelleratorId INT REFERENCES Accelerators(Id),
+	AcceleratorId INT REFERENCES Accelerators(Id),
 	ProjectId INT REFERENCES Projects(Id)
 );
 
 CREATE TABLE Countries (
 	Id SERIAL PRIMARY KEY,
 	Country varchar(20) NOT NULL UNIQUE,
-	Population int NOT NULL,
+	Population int NOT NULL CHECK(Population > 0),
 	GDP int NOT NULL
 );
+	
 
 CREATE TABLE Hotels (
 	Id SERIAL PRIMARY KEY,
 	Hotel varchar(30) NOT NULL UNIQUE,
-	Capacity int NOT NULL
+	City varchar(30) NOT NULL,
 );
 
 CREATE TABLE Fields (
@@ -34,8 +35,10 @@ CREATE TABLE Fields (
 
 CREATE TABLE Scientists (
 	Id SERIAL PRIMARY KEY,
+	FirstName varchar(20) NOT NULL,
+	LastName varchar(20) NOT NULL,
 	DateOfBirth TIMESTAMP NOT NULL,
-	Gender varchar(1) NOT NULL CHECK(Gender IN ('M', 'Ž')),
+	Gender varchar(1) NOT NULL CHECK(Gender IN ('M', 'Ž', 'N', 'O')),
 	CountryId INT REFERENCES Countries(Id),
 	FieldId INT REFERENCES Fields(Id),
 	HotelId INT REFERENCES Hotels(Id)
@@ -45,7 +48,7 @@ CREATE TABLE Scientists (
 CREATE TABLE Researches (
 	Id SERIAL PRIMARY KEY,
 	Name varchar(30) NOT NULL UNIQUE,
-	AmountOfCitations int NOT NULL,
+	AmountOfCitations int NOT NULL CHECK(AmountOfCitations >= 0),
 	DateOfPublishment TIMESTAMP NOT NULL,
 	ProjectId INT REFERENCES Projects(Id)
 );
