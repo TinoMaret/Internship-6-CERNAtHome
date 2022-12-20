75c177b62496016aeac95a4fb7e5d9a57d709eb0
Tino Maretic <pripravnik-tino.maretic@dump.hr>
2022-12-20 22:13:31 +0100

    Added commands to show wanted results and updated name of database

---
+++ CernAtHome_Tasks.sql
@@ -0,0 +1,50 @@
+SELECT r.Name, r.DateOfPublishment,
+	(SELECT STRING_AGG(CONCAT(sc.LastName, ',', LEFT(sc.FirstName, 1)), ';') FROM Scientists sc) AS Scientists 
+FROM Researches r
+JOIN Signature s ON r.Id = s.ResearchId
+JOIN Scientists sc ON sc.Id = s.ScientistId;
+
+SELECT sc.FirstName, sc.LastName, 
+	CASE WHEN sc.Gender = 'M' THEN 'MUŠKI'
+	WHEN sc.Gender = 'Ž' THEN 'ŽENSKI'
+	WHEN sc.Gender = 'N' THEN 'NEPOZNATO'
+	WHEN sc.Gender = 'O' THEN 'OSTALO' END AS Spol,
+	c.Country, c.GDP
+FROM Scientists sc
+JOIN Countries c ON c.Id = sc.CountryId;
+
+SELECT p.Project,
+COALESCE(CAST(
+	(SELECT a.Name FROM Accelerators a
+	 	WHERE a.Id IN (SELECT ap.AcelleratorId FROM AcceleratorProject ap WHERE ap.ProjectId = p.id)) AS VARCHAR), 'NEMA GA') AS Accelerator
+FROM Projects p;
+
+SELECT DISTINCT(p.Id), p.Project FROM Projects p
+JOIN Researches r ON p.Id = r.ProjectId
+WHERE DATE_PART('year',r.DateOfPublishment) BETWEEN 2015 AND 2017;
+
+SELECT DISTINCT(c.Country), COUNT(DISTINCT r.Id) AS BROJRADOVA FROM Countries c
+JOIN Scientists s ON s.CountryId = c.Id
+JOIN Signature sg ON sg.ScientistId = s.Id
+JOIN Researches r ON r.Id = sg.ResearchId
+GROUP BY c.Country
+ORDER BY c.Country;
+
+SELECT DISTINCT (c.Country), r.DateOfPublishment FROM Countries c
+JOIN Scientists s ON s.CountryId = c.Id
+JOIN Signature sg ON sg.ScientistId = s.Id
+JOIN Researches r ON r.Id = sg.ResearchId
+GROUP BY c.Country, r.DateOfpublishment
+ORDER BY r.DateOfPublishment DESC
+LIMIT 1;
+
+SELECT DISTINCT h.City, COUNT(*) AS Scientists_Living FROM Hotels h
+JOIN Scientists s ON s.HotelId = h.Id
+GROUP BY h.City
+ORDER BY Scientists_Living DESC;
+
+SELECT DISTINCT (a.Name), ROUND(AVG(r.AmountOfCitations), 2) AS AverageAmountOfCitations FROM Accelerators a
+JOIN AcceleratorProject ap ON ap.AcelleratorId = a.Id
+JOIN Projects p ON p.Id = ap.ProjectId
+JOIN Researches r ON r.ProjectId = p.Id
+GROUP BY a.Name;
